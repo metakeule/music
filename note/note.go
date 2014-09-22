1b11bb2b009b1ecb00a09e5c345a710ce226a36f
Marc Rene Arns <arns@stridor.net>
2014-09-22 08:27:53 +0200

    cleanup note

--- note/note.go
+++ note/note.go
@@ -2,15 +2,7 @@ package note
 
 import "math"
 
-/*
-from supercollider/include/plugin_interface/SC_InlineUnaryOp.h
-
-inline float32 sc_midicps(float32 note)
-{
-	return (float32)440. * std::pow((float32)2., (note - (float32)69.) * (float32)0.083333333333);
-}
-*/
-const midiCpsFactor = 1/300.0 + 0.08
+type Note float64
 
 const (
 	C0 Note = (12 + iota)
@@ -135,114 +127,23 @@ const (
 	B9
 )
 
-func MidiCps(note float64) float64 {
-	return 440.0 * math.Pow(2.0, (note-69.0)*midiCpsFactor)
-}
-
-type Note float64
-
-var (
 /*
-	C   = C4
-	Cis = Cis4
-	Des = Cis
-	D   = D4
-	Dis = Dis4
-	Es  = Dis
-	E   = E4
-	Eis = F
-	Fes = E
-	F   = F4
-	Fis = Fis4
-	Ges = Fis
-	G   = G4
-	Gis = Gis4
-	As  = Gis
-	A   = A4
-	Ais = Ais4
-	Bb  = Ais
-	B   = B4
-	Ces = B
+from supercollider/include/plugin_interface/SC_InlineUnaryOp.h
+
+inline float32 sc_midicps(float32 note)
+{
+	return (float32)440. * std::pow((float32)2., (note - (float32)69.) * (float32)0.083333333333);
+}
 */
 
-/*
-	C   Note = 60.0 //(60 + iota) // c'
-	Cis Note = 61.0 //                  // cis '
-	D   Note = 62.0 // d'
-	Dis Note = 63.0 // dis'
-	E   Note = 64.0 // ...
-	F   Note = 65.0
-	Fis Note = 66.0
-	G   Note = 67.0
-	Gis Note = 68.0
-	A   Note = 69.0
-	Ais Note = 70.0
-	B   Note = 71.0 // german: H
-*/
-)
+func (n Note) Frequency() float64         { return MidiCps(float64(n)) }
+func (n Note) Transpose(add float64) Note { return Note(float64(n) + add) }
+func (n Note) Octave(num int) Note        { return n.Transpose(float64(num * 12)) }
+func (n Note) Params() map[string]float64 { return map[string]float64{"freq": n.Frequency()} }
 
-func (n Note) Frequency() float64 {
-	return MidiCps(float64(n))
-}
+const midiCpsFactor = 1/300.0 + 0.08
 
-func (n Note) Transpose(add float64) Note {
-	return Note(float64(n) + add)
-}
-
-// Octave transposes the note starting from Octave 4
-// e.g. Octave(2) would transpose 24 semitones up while
-// Octave(-1) would transpose 12 semitones down
-func (n Note) Octave(num int) Note {
-	return n.Transpose(float64(num * 12))
-}
-
-func (n Note) O0() Note {
-	return n.Octave(-4)
-}
-
-func (n Note) O1() Note {
-	return n.Octave(-3)
-}
-
-func (n Note) O2() Note {
-	return n.Octave(-2)
-}
-
-func (n Note) O3() Note {
-	return n.Octave(-1)
-}
-
-func (n Note) O4() Note {
-	return n
-}
-
-func (n Note) O5() Note {
-	return n.Octave(1)
-}
-
-func (n Note) O6() Note {
-	return n.Octave(2)
-}
-
-func (n Note) O7() Note {
-	return n.Octave(3)
-}
-
-func (n Note) O8() Note {
-	return n.Octave(4)
-}
-
-func (n Note) O9() Note {
-	return n.Octave(5)
-}
-
-func (n Note) Params() map[string]float64 {
-	return map[string]float64{"freq": n.Frequency()}
-}
-
-type ChromaticScale struct {
-	BaseNote Note
-}
+func MidiCps(note float64) float64 { return 440.0 * math.Pow(2.0, (note-69.0)*midiCpsFactor) }
 
 // a1 = 440hz = 69 (midi note)
 /*
